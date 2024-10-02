70074fd9780c7b8ff663df222ae7c6d7ca583bc3
Yarden Ron <yardenron4@gmail.com>
2024-10-02 17:42:52 +0300

    Added all calculator buttons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,19 +5,30 @@ import Screen from './Screen';
 import ButtonBox from './ButtonBox';
 import Button from './Button';
 
+const buttonsValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
 function App() {
   return (
     <div>
       <Wrapper>
-        <Screen value="Yarden's Application" />
+        <Screen value="0" />
         <ButtonBox>
-          <Button
-            className=""
-            value="0"
-            onClick={() => {
-              console.log("Button clicked!");
-            }}
-          />
+          {
+            buttonsValues.flat().map((buttonVal, i) => 
+              <Button 
+                key={i}
+                className={buttonVal === "=" ? "equals" : ""} 
+                value={buttonVal.toString()} 
+                onClick={() => console.log(`Button ${buttonVal} clicked!`)} 
+              />
+            )
+          }
         </ButtonBox>
       </Wrapper>
     </div>
